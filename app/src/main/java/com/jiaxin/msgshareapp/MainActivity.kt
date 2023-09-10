bc7deaf4276e9e6d949608cc4c24a662f9ab76e8
knmfr <chenjiax@bu.edu>
2023-09-10 12:19:59 -0400

    Second commit of video8 branch

--- app/src/main/java/com/jiaxin/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/jiaxin/msgshareapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.jiaxin.msgshareapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnShowToast=findViewById<Button>(R.id.btnShowToast)
+        btnShowToast.setOnClickListener {
+            Log.i("MainActivity","Button was clicked !")
+
+            Toast.makeText(this,"Button was clicked !",Toast.LENGTH_SHORT).show()
+        }
     }
 }
