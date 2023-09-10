31d673c49084e3e765abb635f1b233b872e9ed34
knmfr <chenjiax@bu.edu>
2023-09-10 12:52:36 -0400

    First commit of video9 branch

--- app/src/main/java/com/jiaxin/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/jiaxin/msgshareapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.jiaxin.msgshareapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -14,9 +16,17 @@ class MainActivity : AppCompatActivity() {
         val btnShowToast=findViewById<Button>(R.id.btnShowToast)
         btnShowToast.setOnClickListener {
             Log.i("MainActivity","Button was clicked !")
-
             Toast.makeText(this,"Button was clicked !",Toast.LENGTH_SHORT).show()
         }
+        val btnSendMsgNextActivity=findViewById<Button>(R.id.btnSendMsgToNextActivity)
+        val etUserMessage=findViewById<EditText>(R.id.etUserMessage)
+
+        btnSendMsgNextActivity.setOnClickListener {
+            val message:String=etUserMessage.text.toString()
+            Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+
+            val intent= Intent(this,SecondActivity::class.java)//kotlin reflection
+            startActivity(intent)
+        }
     }
-    //
 }
