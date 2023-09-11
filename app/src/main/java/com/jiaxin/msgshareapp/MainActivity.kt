6d732a7582d4524e6ea391fb96be48f0ea2da601
knmfr <chenjiax@bu.edu>
2023-09-10 21:26:05 -0400

    1st commit of video12 branch

--- app/src/main/java/com/jiaxin/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/jiaxin/msgshareapp/MainActivity.kt
@@ -42,7 +42,6 @@ class MainActivity : AppCompatActivity() {
             intent.type="text/plain"
 
             startActivity(Intent.createChooser(intent,"Please select app  : "))
-            //
 
         }
     }
