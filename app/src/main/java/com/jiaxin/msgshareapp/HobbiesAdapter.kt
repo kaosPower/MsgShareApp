1202548f986adaf7b519780e3c0937844f4b0a3f
kaosPower <chenjiax@bu.edu>
2023-09-12 16:35:45 -0400

    2nd commit of video16

--- app/src/main/java/com/jiaxin/msgshareapp/HobbiesAdapter.kt
+++ app/src/main/java/com/jiaxin/msgshareapp/HobbiesAdapter.kt
@@ -1,10 +1,13 @@
 package com.jiaxin.msgshareapp
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class HobbiesAdapter(val context: Context, val hobbies: List<Hobby>) : RecyclerView.Adapter<HobbiesAdapter.MyViewHolder> (){
@@ -24,9 +27,32 @@ class HobbiesAdapter(val context: Context, val hobbies: List<Hobby>) : RecyclerV
     }
 
     inner class MyViewHolder( val itemView: View) : RecyclerView.ViewHolder(itemView){
+        var currentHobby:Hobby?=null
+        var currentPosition: Int=0
+        init{
+            itemView.setOnClickListener{
+                Toast.makeText(context,currentHobby!!.title+"Clicked ! ",Toast.LENGTH_SHORT).show()
+            }
+            val imgShare=itemView.findViewById<ImageView>(R.id.imgShare)
+            imgShare.setOnClickListener {
+                val message:String="My hobby is: "+currentHobby!!.title
+
+                val intent= Intent()
+                intent.action= Intent.ACTION_SEND
+
+                intent.putExtra(Intent.EXTRA_TEXT,message)
+                intent.type="text/plain"
+
+                context.startActivity(Intent.createChooser(intent,"Please select app  : "))
+            }
+
+        }
         fun setData(hobby:Hobby?,pos:Int){
             val txvTitle=itemView.findViewById<TextView>(R.id.txvTitle)
             txvTitle.text=hobby!!.title
+
+            this.currentHobby=hobby
+            this.currentPosition=pos
         }
 
     }
